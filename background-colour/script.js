211e4d1b316f0aaf3ae82e2c20f9b98e1847c42d
Andrew Moore <hello@andrewjamesmoore.com>
2022-03-22 09:43:43 -0600

    First Pass

--- background-colour/script.js
+++ background-colour/script.js
@@ -1,1 +1,14 @@
+"use strict";
 
+const btnColour = document.querySelector(".btnColour");
+const bodyBg = document.querySelector("body");
+
+const colours = ["yellow", "red", "green", "#3b5998"];
+
+const changeColour = () => {
+  //   bodyBg.style.backgroundColour = [];
+  let randomColour = Math.floor(Math.random() * colours.length);
+  bodyBg.style.backgroundColour = colours[randomColour];
+};
+
+btnColour.addEventListener("click", changeColour);
